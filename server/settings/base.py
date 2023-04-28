8e6026a41040241d21b393d342a868ab969a6a43
MarioSilvaPrada <mariosilvaprada@gmail.com>
2023-04-28 09:28:18 +0100

    fix: added logging

--- server/settings/base.py
+++ server/settings/base.py
@@ -178,3 +178,29 @@ CELERY_IMPORTS = ['server.tasks']
 #         'schedule': crontab()  # execute every minute
 #     }
 # }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '[DJANGO] %(levelname)s %(asctime)s %(module)s '
+            '%(name)s.%(funcName)s:%(lineno)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        }
+    },
+    'loggers': {
+        '*': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    },
+}
+
